c7048e857dcb360b60893d52deb0b02de011225c
SayiaFelix <sayia.lucas@student.moringaschool.com>
2022-04-15 11:27:30 +0300

    adding files

--- GitHub-API/src/app/app.module.ts
+++ GitHub-API/src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { LandingpageComponent } from './landingpage/landingpage.component';
+import { ProfileComponent } from './profile/profile.component';
+import { RepoComponent } from './repo/repo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+    NavBarComponent,
+    LandingpageComponent,
+    ProfileComponent,
+    RepoComponent
   ],
   imports: [
     BrowserModule,
