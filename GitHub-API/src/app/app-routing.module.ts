fd028165b963dbd888839c1a2de28fdc115ec88b
SayiaFelix <sayia.lucas@student.moringaschool.com>
2022-04-16 14:50:18 +0300

    Adding gitnubApi service

--- GitHub-API/src/app/app-routing.module.ts
+++ GitHub-API/src/app/app-routing.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { GithubComponent } from './github/github.component';
 import { LandingpageComponent } from './landingpage/landingpage.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 const routes: Routes = [
-
+ { path: '', redirectTo:"/landingpage", pathMatch:"full"},
   { path: 'landingpage', component: LandingpageComponent},
   { path: 'github', component: GithubComponent},
-
+  { path:'**', component:NotFoundComponent}
 ];
 
 @NgModule({
